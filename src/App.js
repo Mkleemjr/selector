d249a2f6339d621a97229282a5fee5f06001e8a4
mkleemoff <mkleemjr@gmail.com>
2020-02-18 20:40:54 -0500

    Added the Shape component and tested the onClick functionality - everything working.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React, {Component} from 'react';
 import Shape from './shape';
 
 class Selector extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            selectedShape: 'square',
+        }
+    }
+
+    // Our methods live here.
+    selectShape = (shapeName) => {
+        this.setState({
+            selectedShape: shapeName,
+        })
+    }
 
     render () {
         return (
             <div className="container">
                 <div className="navbar">
-                    <div>Selected: <span> What goes here!?!?</span></div>
+        <div>Selected: <span>{this.state.selectedShape}</span></div>
                 </div>
                 <div className="shape-list"> 
-                    <Shape shape="square"/>
-                    <Shape shape="triangle"/>
-                    <Shape shape="circle"/>
+                    <Shape shape="square" selectShape={this.selectShape}/>
+                    <Shape shape="triangle" selectShape={this.selectShape}/>
+                    <Shape shape="circle" selectShape={this.selectShape}/>
                 </div>
             </div>
         )
