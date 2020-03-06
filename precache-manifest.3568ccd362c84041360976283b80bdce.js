0883d847bc121ef22cdcaf0c36a7a8a095df3626
mkleemoff <mkleemjr@gmail.com>
2020-03-06 17:28:34 -0500

    Updates

---
+++ precache-manifest.3568ccd362c84041360976283b80bdce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bd247cb1ded677c1bd8b17fcf19090",
+    "url": "/selector/index.html"
+  },
+  {
+    "revision": "86e40ce5e82b062d6ce8",
+    "url": "/selector/static/css/main.6f8210b4.chunk.css"
+  },
+  {
+    "revision": "ba51f8838ec5e8e9309c",
+    "url": "/selector/static/js/2.694c7b11.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/selector/static/js/2.694c7b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e40ce5e82b062d6ce8",
+    "url": "/selector/static/js/main.cf78c113.chunk.js"
+  },
+  {
+    "revision": "c1069b8034f178e38c5c",
+    "url": "/selector/static/js/runtime-main.342e84bb.js"
+  }
+]);
